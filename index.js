d04d2db4e0ece5706c5ebddce4b6051c0e7b0ea8
Laurel Rubel <laurel.rubel@gmail.com>
2023-04-29 19:09:29 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.round(Math.random() * 10);
+
+const num3 = 29;
+const num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20);
